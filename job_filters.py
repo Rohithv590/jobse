857c207ea400fcbbfb614c04130f9148366cafa5
Rohithv590 <247r5a0544@cmrtc.ac.in>
2026-06-14 01:00:33 +0530

    Create job_filters.py

---
+++ job_filters.py
@@ -0,0 +1,30 @@
+TARGET_ROLES = [
+    "software engineer",
+    "java developer",
+    "backend developer",
+    "full stack developer",
+    "associate software engineer",
+    "graduate engineer trainee",
+    "trainee software engineer"
+]
+
+REJECT_KEYWORDS = [
+    "training fee",
+    "registration fee",
+    "investment",
+    "crypto",
+    "forex",
+    "mlm",
+    "data entry"
+]
+
+SKILLS = [
+    "java",
+    "node.js",
+    "express",
+    "mysql",
+    "docker",
+    "git",
+    "flutter",
+    "python"
+]
